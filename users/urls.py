831133f5882eec475e802683660287bc7eaeb772
askip <a.skiper2013@yandex.ru>
2023-08-21 15:43:03 +0300

    добавление users

--- users/urls.py
+++ users/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
+from rest_framework_simplejwt.views import TokenRefreshView
 
 from users.apps import UsersConfig
+from users.views import *
 
 app_name = UsersConfig.name
 
