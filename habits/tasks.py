c52663ce79b740073c0eb3a7395c5a3374403cb6
askip <a.skiper2013@yandex.ru>
2023-08-22 17:10:44 +0300

    Исправление ошибок

--- habits/tasks.py
+++ habits/tasks.py
@@ -3,11 +3,10 @@ from config import settings
 from config.celery import app
 from habits.models import Habit
 
-
 @app.task
-def send_telegram_message(habits_id):
+def send_telegram_message(habit_id):
     """Отправка сообщения через бот TG"""
-    habit = Habit.objects.get(id=habits_id)
+    habit = Habit.objects.get(id=habit_id)
     bot = TeleBot(settings.TG_BOT_TOKEN)
     message = f"Напоминание о выполнении привычки {habit.action} в {habit.time} в {habit.place}"
     bot.send_message(habit.owner.chat_id, message)
--- users/models.py
+++ users/models.py
@@ -2,7 +2,6 @@ from django.contrib.auth.models import AbstractUser
 from django.db import models
 from users.services import UserCManager
 from django.utils.translation import gettext_lazy as _
-
 NULLABLE = {'null': True, 'blank': True}
 
 
@@ -17,9 +16,8 @@ class User(AbstractUser):
     phone = models.CharField(max_length=35, verbose_name='телефон', **NULLABLE)
     avatar = models.ImageField(upload_to='users/', verbose_name='аватар', **NULLABLE)
     city = models.CharField(max_length=100, verbose_name='Город', **NULLABLE)
-    role = models.CharField(max_length=10, choices=UserRoles.choices, default=UserRoles.MEMBER, verbose_name='роль',
-                            **NULLABLE)
-    is_active = models.BooleanField(default=True, verbose_name='активность')
+    role = models.CharField(max_length=10, choices=UserRoles.choices, default=UserRoles.MEMBER, verbose_name='роль', **NULLABLE)
+    is_active = models.BooleanField(default=True,  verbose_name='активность')
     chat_id = models.CharField(max_length=15, verbose_name='ID бота TG', **NULLABLE)
 
     USERNAME_FIELD = "email"
